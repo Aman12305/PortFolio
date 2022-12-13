27b8a09451381e5ebecd177854502e70430028d8
unknown <amansolanki2305@gmail.com>
2022-12-13 23:35:47 +0530

    font updated

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -6,11 +6,7 @@ export default function Loading() {
   return (
     <div className='universalbox backgroundimage'
     style={{
-        backgroundImage: `linear-gradient(rgb(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(
-       "https://cdn.pixabay.com/photo/2016/09/08/20/52/starry-sky-1655503__480.jpg"
-        )
-        `,
-        backgroundRepeat:"no-repeat"
+        
     }} 
     >
     <img className='loadingimage' src="./img/loading.gif" alt="loading"></img>
